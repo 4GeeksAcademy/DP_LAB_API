9a2abb040146e5fd5981de786cbddc052164fe65
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-10-17 19:02:50 +0000

    Db order added

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,43 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+        
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, unique=False, nullable=False)
+    number = db.Column(db.String(80), unique=False, nullable=False)
+    order_key = db.Column(db.String(80), unique=False, nullable=False)
+    created_via = db.Column(db.String(80), unique=False, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    date_created = db.Column(db.String(80), unique=False, nullable=False)
+    discount_total = db.Column(db.String(80), unique=False, nullable=False)
+    discount_tax = db.Column(db.String(80), unique=False, nullable=False)
+    shipping_total = db.Column(db.String(80), unique=False, nullable=False)
+    total = db.Column(db.String(80), unique=False, nullable=False)
+    customer_id = db.Column(db.String(80), unique=False, nullable=False)
+    payment_method = db.Column(db.String(80), unique=False, nullable=False)
+    total_tax = db.Column(db.String(80), unique=False, nullable=False)
+    
+    
+   
+    def __repr__(self):
+        return f'<Order {self.order}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "parent_id": self.parent_id,
+            "number": self.number,
+            "order_key": self.order_key,
+            "created_via": self.created_via,
+            "status": self.status,
+            "date_created": self.date_created,
+            "discount_total": self.discount_total,
+            "discount_tax": self.discount_tax,
+            "shipping_total": self.shipping_total,
+            "total": self.total,
+            "customer_id": self.customer_id,
+            "payment_method": self.payment_method,
+            "total_tax": self.total_tax,
+            # do not serialize the password, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Order
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -14,6 +14,15 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/order', methods=['POST', 'GET'])
+def get_orders():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
